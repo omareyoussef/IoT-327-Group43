bef6d8dc1fec6d7528965759a4f648ebc6819bac
Jeff Kim <98762871+20018040@users.noreply.github.com>
2024-12-03 16:29:29 -0800

    Add files via upload
    comments and fixed the time error caused by minutes = 10800. ( changed to minutes = 180).

--- mongoDB.py
+++ mongoDB.py
@@ -8,8 +8,7 @@ import certifi
 
 DBName = "test" 
 connectionURL = "mongodb+srv://jeffkim:lWRrXOkPvEjzvO3Z@2024fall327.aemto.mongodb.net/?retryWrites=true&w=majority&appName=2024Fall327" 
-virtualTable = "table_virtual" 
-ids = {"zwl-0y3-7ob-93e", "c84d3021-5582-402e-b3ae-2118ff775916","bb113181-961a-43ed-9f35-2a2c9dd74616"}
+virtualTable = "mongo_virtual" 
 
 def QueryToList(query):
 	# Iterate through the query and put it in a list.
@@ -32,26 +31,21 @@ def QueryDatabase() -> list[dict]:
 		cluster = connectionURL
 		client = MongoClient(connectionURL, tlsCAFile=certifi.where())
 		db = client[DBName]
-		print("Database collections: ", db.list_collection_names())
 
 		#We first ask the user which collection they'd like to draw from.
 		virtualTableObject = db[virtualTable]
-		print("Table:", virtualTableObject)
 
 		#We convert the cursor that mongo gives us to a list for easier iteration.
-		# Convert the time now in the current location to UTC, then subtract 5 minutes to get all the documents generated 
-		# in the past 5 minutes.
-		timeCutOff = datetime.now(timezone.utc) - timedelta(minutes=10800)
-		timeQuery = virtualTableObject.find({"time":{"$gte":timeCutOff},"payload.parent_asset_uid": {"$in": list(ids)}})
-		# idQuery = timeQuery.find({"parent_asset_uid": {"$in": ids}})
+		# Convert the time now in the current location to UTC, then subtract 180 minutes to get all the documents generated 
+		# in the past 3hours
+		timeCutOff = datetime.now(timezone.utc) - timedelta(minutes=180)
+		timeQuery = virtualTableObject.find({"time":{"$gte":timeCutOff}})
 		currentDocuments = QueryToList(timeQuery)
 		return currentDocuments
 		
-
-
+    #if connection is not built or couldnt retrieve any data, runs exception.
 	except Exception as e:
 		print("Please make sure that this machine's IP has access to MongoDB.")
 		print("Error:",e)
 		exit(0)
-		
-print(QueryDatabase())
+	
